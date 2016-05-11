0823aeef4d0b66b235f72cdd1ec89c7e2aa73444
brokenBrain <eddyhudson@utexas.edu>
2016-05-10 21:18:01 -0500

    Added convex optimizer: MALLET

--- stanford_parser/stanford-parser-full-2015-12-09/KleinBilingualParser.java
+++ stanford_parser/stanford-parser-full-2015-12-09/KleinBilingualParser.java
@@ -1,4 +1,3 @@
-
 import edu.stanford.nlp.parser.lexparser.BinaryGrammar;
 import edu.stanford.nlp.parser.lexparser.DependencyGrammar;
 import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
@@ -37,6 +36,9 @@ import java.io.*;
 import java.util.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+
+// MALLET Shit
+import cc.mallet.optimize.*;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -276,6 +278,31 @@ int i = 0;
       }
       System.out.println("=================================");
     }
+        
+        
+        ///////////////////////
+        //
+        //  MALLET trial run
+        //
+        ///////////////////////
+        
+        OptimizerExample optimizable = new OptimizerExample(0, 0);
+        Optimizer optimizer = new LimitedMemoryBFGS(optimizable);
+        
+        boolean converged = false;
+        
+        try {
+            converged = optimizer.optimize();
+        } catch (IllegalArgumentException e) {
+            // This exception may be thrown if L-BFGS
+            //  cannot step in the current direction.
+            // This condition does not necessarily mean that
+            //  the optimizer has failed, but it doesn't want
+            //  to claim to have succeeded...
+        }
+        
+        System.out.println(optimizable.getParameter(0) + ", " +
+                           optimizable.getParameter(1));
 
   } // end main
     
@@ -334,7 +361,64 @@ private static HashMap<Tree, Tree> getSampleNodeAlignment(Tree eParseTree, Tree 
 
 /////////////////////////
 /////////////////////////
-
+    
+    
+/////////////////////////
+//
+//  MALLET
+//
+/////////////////////////
+    
+    private static class OptimizerExample implements Optimizable.ByGradientValue {
+        
+        // Optimizables encapsulate all state variables,
+        //  so a single Optimizer object can be used to optimize
+        //  several functions.
+        
+        double[] parameters;
+        
+        public OptimizerExample(double x, double y) {
+            parameters = new double[2];
+            parameters[0] = x;
+            parameters[1] = y;
+        }
+        
+        public double getValue() {
+            
+            double x = parameters[0];
+            double y = parameters[1];
+            
+            return -3*x*x - 4*y*y + 2*x - 4*y + 18;
+            
+        }
+        
+        public void getValueGradient(double[] gradient) {
+            
+            gradient[0] = -6 * parameters[0] + 2;
+            gradient[1] = -8 * parameters[1] - 4;
+            
+        }
+        
+        // The following get/set methods satisfy the Optimizable interface
+        
+        public int getNumParameters() { return 2; }
+        public double getParameter(int i) { return parameters[i]; }
+        public void getParameters(double[] buffer) {
+            buffer[0] = parameters[0];
+            buffer[1] = parameters[1];
+        }
+        
+        public void setParameter(int i, double r) {
+            parameters[i] = r;
+        }
+        public void setParameters(double[] newParameters) {
+            parameters[0] = newParameters[0];
+            parameters[1] = newParameters[1];
+        }
+    }
+    
+/////////////////////////
+/////////////////////////
 
 
 ///////////////////////////////
@@ -379,4 +463,5 @@ private static HashMap<Tree, Tree> getSampleNodeAlignment(Tree eParseTree, Tree 
     }
     op.tlpParams.display();
   }
+    
 }
